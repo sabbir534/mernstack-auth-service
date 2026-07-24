f002e6b04e05ca0ac30ac92e0ccd5f99a9cd48c2
Sabbir Ahmed <prince534534@gmail.com>
2026-07-24 17:46:53 +0600

    test lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -28,6 +28,7 @@ export default defineConfig([
     },
     rules: {
       'no-console': 'off',
+      'dot-notation': 'error',
     },
   },
 ]);
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 function welcome(name: string) {
   //return `Hello, ${name}!`;
   console.log(name);
+  const user = {
+    fname: 'Ahmed',
+  };
+  return user.fname;
 }
 
 welcome('Sabbir');
